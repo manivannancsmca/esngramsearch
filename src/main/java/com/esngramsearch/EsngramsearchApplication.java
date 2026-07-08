a5bb6a35f96fc63279edd3028c03b80525109a24
manivannancsmca <manivannancsmca@gmail.com>
2026-07-08 23:08:09 +0530

    first commit

---
+++ src/main/java/com/esngramsearch/EsngramsearchApplication.java
@@ -0,0 +1,13 @@
+package com.esngramsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsngramsearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsngramsearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/esngramsearch/EsngramsearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esngramsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsngramsearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
