473de5b21a47463b3c6c3a567e63744eb26826c9
manivannancsmca <manivannancsmca@gmail.com>
2026-07-08 23:33:18 +0530

    elasticsearch setting json file issue fixed

--- src/main/java/com/esngramsearch/EsngramsearchApplication.java
+++ src/main/java/com/esngramsearch/EsngramsearchApplication.java
@@ -2,8 +2,10 @@ package com.esngramsearch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 @SpringBootApplication
+@EnableElasticsearchRepositories(basePackages = "com.esngramsearch.repository")
 public class EsngramsearchApplication {
 
 	public static void main(String[] args) {
